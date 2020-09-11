e8c23a7f8ea5468442d33c926029e712e2eff3fc
Murat <qewda>
2020-09-11 14:01:01 +0300

    Practice3.1

--- src/ru/mirea/Circle.java
+++ src/ru/mirea/Circle.java
@@ -4,24 +4,28 @@ public class Circle {
     private int radius;
     private int xPos =0;
     private int yPos =0;
-    public final double lenght = 2 * Math.PI * radius ;
+    private  double lenght;
 
     public Circle(int radius, int xPos, int yPos) {
         this.radius = radius;
         this.xPos = xPos;
         this.yPos = yPos;
+        calculenght();
     }
 
     public Circle(int radius) {
         this.radius = radius;
+        calculenght();
     }
 
     public int getxPos() {
-        return xPos;
+        return xPos;.
+
     }
 
     public void setxPos(int xPos) {
         this.xPos = xPos;
+
     }
 
     public int getyPos() {
@@ -37,5 +41,21 @@ public class Circle {
     }
     public double getSpace(){
      return lenght*radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", xPos=" + xPos +
+                ", yPos=" + yPos +
+                ", lenght=" + lenght +
+                '}';
+    }
+    public void printCircle(){
+        System.out.println(this);
+    }
+    private void calculenght(){
+        lenght = 2 * Math.PI * radius ;
     }
 }
---
+++ src/ru/mirea/Tester.java
@@ -0,0 +1,15 @@
+package ru.mirea;
+
+public class Tester {
+
+    public static void main (String[] args){
+      //practice 3
+        Circle circle = new Circle(10);
+        circle.printCircle();
+
+        Circle circle1 = new Circle(15,10 ,34);
+        circle1.printCircle();
+    }
+
+    }
+
